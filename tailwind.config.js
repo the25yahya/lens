7a40573dbfb390f59133c3464f988b9c52b7595a
the25yahya <114318659+the25yahya@users.noreply.github.com>
2024-01-03 15:06:31 +0100

    initilized repo

---
+++ tailwind.config.js
@@ -0,0 +1,66 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [    "./index.html",
+  "./src/**/*.{js,ts,jsx,tsx}",],
+  theme: {
+    extend: {
+      spacing: {
+        '128': '15rem',
+        '100' : '12rem',
+        '50' : '8rem',
+        '30' : '20%'
+      },
+      transform: {
+      
+      },
+      colors: {
+        'custom-color': '#f2f0ea',
+        'custom-color2': '#010101',
+        'custom-color3': '#edcf5d',
+        'custom-color4': '#a4a4a4',
+        'custom-color5':'#383673',
+      },
+      width: {
+        1000:'1000px',
+        900:'900px',
+        800:"800px",
+        700:"700px",
+        600:"600px",
+        500: '500px',
+        420: '420px',
+        410: '410px',
+        400: '400px',
+        350: '350px',
+        320: '320px',
+        300: '300px',
+        200: '200px',
+        250: '250px',
+        180: "180px",
+        100: '100px',
+        60: '60px',
+        50: '50px',
+        40: '40px',
+        30: '30px',
+      },
+      height: {
+        800:"800px",
+        700:"700px",
+        600:"600px",
+        500: '500px',
+        420: '420px',
+        410: '410px',
+        400: '400px',
+        300: '300px',
+        200: '200px',
+        250: '250px',
+        180: "180px",
+        100: '100px',
+        60: '60px',
+        50: '50px',
+        40: '40px',
+        30: '30px',
+      },
+    },
+  },
+  plugins: [],
+}
