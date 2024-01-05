97d892d3dce1d312c9b9eb739e6a03e4076fa2cb
the25yahya <114318659+the25yahya@users.noreply.github.com>
2024-01-05 16:12:38 +0100

    added section + footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,8 +1,23 @@
+import { F } from 'core-js/modules/_export'
 import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className='bg-pink-100 flex items-center justify-around mt-100 p-20'>
+     <h1 className='title text-3xl cursor-pointer'>Lens</h1>
+     <div className='flex flex-col items-start'>
+      <p className='mb-3 font-mono font-thin text-lg cursor-pointer'>FAQ</p>
+      <p className='mb-3 font-mono font-thin text-lg cursor-pointer'>PRIVACY POLICY</p>
+      <p className='mb-3 font-mono font-thin text-lg cursor-pointer'>TERMS & CONDITIONS</p>
+      <p className='mb-3 font-mono font-thin text-lg cursor-pointer'>DELIVERY</p>
+      <p className='mb-3 font-mono font-thin text-lg cursor-pointer'>CONACT US</p>
+     </div>
+     <div className='flex flex-col items-start'>
+      <p className='mb-5 font-mono'>MAILING LIST</p>
+      <input className='w-400 px-12 py-4 bg-gray-100 font-mono font-thin mb-4 opacity-90' placeholder='ENTER YOUR NAME'/>
+      <button className='w-400 px-12 py-4 text-white bg-black font-mono font-thin'>SIGN UP FOR DISCOUNTS</button>
+     </div>
+    </footer>
   )
 }
 
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -9,16 +9,17 @@ const Home = () => {
   console.log(isMenuOpen);
   return (
     <div className='flex flex-col'>
-     <div className={`absolute pt-10 flex flex-col items-center text-center bg-custom-color w-full h-full z-10 transition-div ${isMenuOpen ? '' : 'hidden'}`}>
+     <div className={`absolute pt-10 flex flex-col items-center text-center bg-custom-color w-full h-full z-50 transition-div ${isMenuOpen ? '' : 'hidden'}`}>
       <p className='font-mono text-white mt-40 text-xl'>MENU</p>
       <div className='text-white'>
         <p className='text-5xl mt-2 font-semibold'>Products</p>
         <p className='font-semibold text-5xl mt-5'>Brands</p>
         <p className='text-5xl mt-5 font-semibold'>About</p>
-        <p className='text-5xl mt-5 font-semibold'>Sale</p>
+        <p className='text-5xl mt-5 font-semibold'>Men eyewear</p>
+        <p className='text-5xl mt-5 font-semibold'>Women eyewear</p>
       </div>
      </div>
-     <section className='flex items-center justify-between border-b-2 border-black'>
+     <section className='flex items-center justify-between border-b border-gray-200'>
         <div className='flex flex-col items-center pl-12 ml-12 mt-10 relative'>
             <div className='absolute flex rotate-90 -left-8 top-60'>
              <div className='font-bold text-5xl text-gray-400 cursor-pointer'>.</div>
@@ -64,18 +65,44 @@ const Home = () => {
      </section>
      <section className='mt-20'>
       <div className='flex items-center justify-center relative'>
-        <img className='w-400' src='https://i.pinimg.com/736x/ac/6c/9f/ac6c9fec24303d2a8fc04d3aec1bf0c2.jpg'/>
-        <div className='p-10 text-center'>
-         <h1 className='font-mono text-5xl font-thin mb-10'>The difference is in the lens</h1>
-         <p>through endeless reasearch with physical photo filters and digital manipulation techniques we discoverd our perfect lens. We found a way to use natural sunlight to transform the world around you into a vivid picture; an experience full of clarity and coulour making everyday life look ten times better</p>
-        </div>
-        <img className='w-380' src='https://i.pinimg.com/736x/55/1f/2f/551f2fbd4081135020d26b3d874f0399.jpg' />
-      </div>
-      <div className='mt-100 flex items-center'>
-      <img
-          className='absolute -bottom-100 w-250'
+        <img className='w-400 h-555' src='https://img.vitkac.com/uploads/product_thumb/OKULARY%20CHARLIE%200-C154/up/2.jpg'/>
+        <div className='p-10 px-18 text-center bg-zinc-100 h-555 flex flex-col items-center relative'>
+         <div className='absolute -bottom-128 flex items-center ml-72 w-600'>
+         <img
+          className='w-250'
           src='https://i.pinimg.com/originals/5d/a2/3b/5da23b45565e70f656b50c4d4409e46c.jpg'
         />
+         <div className='flex- flex-col ml-10'>
+         <p className='mb-3 font-semibold'>They enhance real life like no other sunglasses i`ve ever seen</p>
+        <p><span className='text-cyan-400 font-semibold'>@maggsdalzell</span> on instagram</p>
+         </div>
+         </div>
+         <h1 className='font-mono text-4xl font-thin mb-10 mt-10 w-400'>The difference is in the lens</h1>
+         <p className='w-400 font-semibold'>through endeless reasearch with physical photo filters and digital manipulation techniques we discoverd our perfect lens. We found a way to use natural sunlight to transform the world around you into a vivid picture; an experience full of clarity and coulour making everyday life look ten times better</p>
+        </div>
+        <img className='w-380 h-555' src='https://i.pinimg.com/736x/55/1f/2f/551f2fbd4081135020d26b3d874f0399.jpg' />
+      </div>
+      <div className='flex items-start justify-center mt-140'>
+       <div className='relative'>
+        <img
+          className='w-400'
+          src='https://img.freepik.com/premium-photo/stylish-man-sunglasses-hat_85574-13912.jpg?size=626&ext=jpg&ga=GA1.1.1826414947.1699833600&semt=ais'
+        />
+        <div className='flex- flex-col   ml-10 absolute right-140 mr-32 text-center bottom-36 w-300'>
+           <p className='mb-3 font-semibold'>wearing under the sunset is the most amazing thing.</p>
+          <p><span className='text-cyan-400 font-semibold'>@KellyLing</span> on instagram</p>
+         </div>
+       </div>
+       <div className='relative'>
+        <img
+          className='w-250 ml-8'
+          src='https://media1.popsugar-assets.com/files/thumbor/ArueK53Zo5lmv6LuMlgvBuuUup0/fit-in/728xorig/filters:format_auto-!!-:strip_icc-!!-/2020/02/07/890/n/1922564/6c3f73985e3dc6d08e4236.78018502_/i/best-sunglasses-for-women.jpg'
+        />
+         <div className='flex- flex-col   ml-10 absolute left-140 bottom-36 w-300 text-center'>
+           <p className='mb-3 font-semibold'>wearing under the sunset is the most amazing thing.</p>
+          <p><span className='text-cyan-400 font-semibold'>@KellyLing</span> on instagram</p>
+         </div>
+       </div>
       </div>
      </section>
     </div>
