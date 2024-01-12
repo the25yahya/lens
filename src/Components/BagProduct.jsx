3054b54dd1142e2d5dad80113c26419060fb67f2
the25yahya <114318659+the25yahya@users.noreply.github.com>
2024-01-12 18:48:23 +0100

    working on bug

--- src/Components/BagProduct.jsx
+++ src/Components/BagProduct.jsx
@@ -3,14 +3,14 @@ import React from 'react'
 
 const BagProduct = (props) => {
   return (
-        <div className='flex items-center'>
-            <div>
+        <div className='flex items-center justify-start'>
+            <div className='border p-2 mr-4'>
                 <img
-                    className='w-8'
+                    className='w-12'
                     src={props.img1}
                 />
             </div>
-            <div className='flex flex-col'>
+            <div className='flex flex-col mr-8'>
                 <h1>{props.name}</h1>
                 <p><span>{props.color} </span>/<span>{props.type}</span></p>
             </div>
