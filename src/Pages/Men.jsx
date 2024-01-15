cafbcffa2f569cb78335d53515d25f2460563343
the25yahya <114318659+the25yahya@users.noreply.github.com>
2024-01-15 15:49:16 +0100

    fixed bug

--- src/Pages/Men.jsx
+++ src/Pages/Men.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
 import { useStateContext } from '../Contexts/ContextProvider'
 import Product from '../Components/Product';
 import FeaturedSelect from '../Components/FeaturedSelect';
 import Filter from '../Components/Filter';
+import ProductDisplay from '../Components/ProductDisplay';
 const Men = () => {
-
-  const {menGlasses} = useStateContext();
+  const {menGlasses,setProductDisplayMen,setDisplayedProduct,scrollToTop,productDisplayMen,displayedProduct} = useStateContext();
   const MenProducts = menGlasses.map((item) =>{
+    const handleDisplayedProduct = () => {
+      setDisplayedProduct(item);
+      setProductDisplayMen(true);
+      scrollToTop();
+    } 
     return(
       <Product
+        onClick={handleDisplayedProduct}
         key={item.name}
         name={item.name}
         img1={item.img1}
@@ -24,15 +29,38 @@ const Men = () => {
       />
     )
   })
-  return (
-    <div className='relative flex justify-start pt-100 px-12'>
-    <FeaturedSelect />
-    <Filter />
-    <div className='flex flex-wrap'>
-     {MenProducts}
-    </div>
-   </div>
-  )
+  if(!productDisplayMen){
+    return (
+      <div className='relative flex justify-start pt-100 px-12'>
+      <FeaturedSelect />
+      <Filter />
+      <div className='flex flex-wrap'>
+       {MenProducts}
+      </div>
+     </div>
+    )
+  } else{
+    return(
+      <div className='relative flex justify-start pt-50 px-12'>
+       <ProductDisplay 
+        key={displayedProduct.name}
+        name={displayedProduct.name}
+        img1={displayedProduct.img1}
+        img2={displayedProduct.img2}
+        img3={displayedProduct.img3}
+        img4={displayedProduct.img4}
+        img5={displayedProduct.img5}
+        img6={displayedProduct.img6}
+        price={displayedProduct.price}
+        description={displayedProduct.description}
+        tag={displayedProduct.tag}
+        colors={displayedProduct.colors}
+        type={displayedProduct.type}
+        color={displayedProduct.colors}
+       />
+     </div>
+    )
+  }
 }
 
 export default Men
