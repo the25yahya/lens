06146b976ccf3b07a220625dabd446bb8ad12f8f
the25yahya <114318659+the25yahya@users.noreply.github.com>
2024-01-15 15:52:41 +0100

    finished product display

--- src/Pages/Women.jsx
+++ src/Pages/Women.jsx
@@ -2,13 +2,20 @@ import React from 'react'
 import { useStateContext } from '../Contexts/ContextProvider'
 import Product from '../Components/Product';
 import FeaturedSelect from '../Components/FeaturedSelect';
+import ProductDisplay from '../Components/ProductDisplay';
 import Filter from '../Components/Filter';
 const Women = () => {
 
-  const {womenGlasses} = useStateContext();
+  const {womenGlasses,setProductDisplayWomen,setDisplayedProduct,scrollToTop,productDisplayWomen,displayedProduct} = useStateContext();
   const WomenProducts = womenGlasses.map((item) =>{
+    const handleDisplayedProduct = () => {
+      setDisplayedProduct(item);
+      setProductDisplayWomen(true);
+      scrollToTop();
+    } 
     return(
       <Product
+      onClick={handleDisplayedProduct}
         key={item.name}
         name={item.name}
         img1={item.img1}
@@ -24,15 +31,38 @@ const Women = () => {
       />
     )
   })
-  return (
-    <div className='relative flex justify-start pt-100 px-12'>
-    <FeaturedSelect />
-    <Filter />
-    <div className='flex flex-wrap'>
-     {WomenProducts}
-    </div>
-   </div>
-  )
+  if(!productDisplayWomen){
+    return (
+      <div className='relative flex justify-start pt-100 px-12'>
+      <FeaturedSelect />
+      <Filter />
+      <div className='flex flex-wrap'>
+       {WomenProducts}
+      </div>
+     </div>
+    )
+  } else{
+    return(
+      <div className='relative flex justify-start pt-50 px-12'>
+       <ProductDisplay 
+        key={displayedProduct.name}
+        name={displayedProduct.name}
+        img1={displayedProduct.img1}
+        img2={displayedProduct.img2}
+        img3={displayedProduct.img3}
+        img4={displayedProduct.img4}
+        img5={displayedProduct.img5}
+        img6={displayedProduct.img6}
+        price={displayedProduct.price}
+        description={displayedProduct.description}
+        tag={displayedProduct.tag}
+        colors={displayedProduct.colors}
+        type={displayedProduct.type}
+        color={displayedProduct.colors}
+       />
+     </div>
+    )
+  }
 }
 
 export default Women
